1cebec6b925253cb75b7a6e2dc92f99bb48efe45
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-07-23 22:35:35 -0300

    ✅ test: create test of integration to `/` route
    update: `.gitignore` file,
    and create: `test/repo.test.js` and `test/mock/index.js` files

---
+++ test/mock/index.js
@@ -0,0 +1,51 @@
+const mockDataApi = {
+  data: [
+    {
+      owner: {
+        avatar_url: 'https://avatars.githubusercontent.com/u/85720722?s=400',
+      },
+      full_name: 'test',
+      description: 'test',
+      language: 'C#',
+      created_at: '2020-01-01',
+    },
+    {
+      owner: {
+        avatar_url: 'https://avatars.githubusercontent.com/u/85720722?s=400',
+      },
+      full_name: 'test',
+      description: 'test',
+      language: 'C#',
+      created_at: '2020-01-01',
+    },
+    {
+      owner: {
+        avatar_url: 'https://avatars.githubusercontent.com/u/85720722?s=400',
+      },
+      full_name: 'test',
+      description: 'test',
+      language: 'C#',
+      created_at: '2020-01-01',
+    },
+    {
+      owner: {
+        avatar_url: 'https://avatars.githubusercontent.com/u/85720722?s=400',
+      },
+      full_name: 'test',
+      description: 'test',
+      language: 'C#',
+      created_at: '2020-01-01',
+    },
+    {
+      owner: {
+        avatar_url: 'https://avatars.githubusercontent.com/u/85720722?s=400',
+      },
+      full_name: 'test',
+      description: 'test',
+      language: 'C#',
+      created_at: '2020-01-01',
+    },
+  ],
+};
+
+module.exports = { mockDataApi };
---
+++ test/repo.test.js
@@ -0,0 +1,53 @@
+const request = require('supertest');
+const app = require('../src/app');
+const axios = require('axios');
+const { mockDataApi } = require('./mock');
+
+describe('GET /', () => {
+  beforeEach(() => {
+    axios.get = jest.fn().mockResolvedValue(mockDataApi);
+  });
+
+  afterEach(() => jest.clearAllMocks());
+
+  describe('when the request is valid', () => {
+    it('should return a 200 status code', async () => {
+      const response = await request(app).get('/');
+      expect(response.status).toBe(200);
+    });
+
+    it('should return a JSON response', async () => {
+      const response = await request(app).get('/');
+      expect(response.type).toBe('application/json');
+    });
+
+    it('should return a list of 5 repos', async () => {
+      const response = await request(app).get('/');
+      expect(response.body.length).toBe(5);
+    });
+
+    it('should return a list of repos with the correct properties', async () => {
+      const response = await request(app).get('/');
+      expect(response.body[0]).toHaveProperty('image');
+      expect(response.body[0]).toHaveProperty('full_name');
+      expect(response.body[0]).toHaveProperty('description');
+      expect(response.body[0]).toHaveProperty('language');
+      expect(response.body[0]).toHaveProperty('created_at');
+    });
+  });
+
+  describe('when the request is invalid', () => {
+    beforeEach(() => {
+      axios.get = jest.fn().mockRejectedValue(new Error());
+    });
+
+    afterEach(() => jest.clearAllMocks());
+
+    it('should return a 500 status code', async () => {
+      const response = await request(app).get('/');
+      console.log(response.body);
+      console.log(response.status);
+      expect(response.status).toBe(500);
+    });
+  });
+});
