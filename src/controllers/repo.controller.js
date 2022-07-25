c150f91183470f75582bd4fc695f691552507195
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-07-24 23:19:32 -0300

    🩺 chore(api): change return api, array to object
    update `repo.controller.js`

--- src/controllers/repo.controller.js
+++ src/controllers/repo.controller.js
@@ -3,7 +3,7 @@ const { getRepoApi } = require('../services/repo.service');
 const getRepo = async (_req, res) => {
   try {
     const repo = await getRepoApi();
-    res.status(200).json(repo);
+    res.status(200).json({ ...repo });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
